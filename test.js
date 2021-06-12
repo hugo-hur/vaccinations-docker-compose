54787d180535f00bb98736e2305278b71b298d44
Hugo Hurskainen <hugo.hurskainen@gmail.com>
2021-06-12 18:10:35 +0300

    Added initial queries and simple display

--- test.js
+++ test.js
@@ -11,11 +11,50 @@
     }
   }
 */
+function isFutureDate(value) {
+  d_now = new Date();
+  d_inp = new Date(value)
+  return d_now.getTime() <= d_inp.getTime();
+}
 
-function ordersQuery(){
+function left(data){
+  data = data.data;
+  header = document.getElementById("total-left");
+  console.log(data.orders.nodes);
+  var used = 0;
+  var arrived = 0;
+  var exp = 0;
+  var unused_not_exp = 0;
+  var unused_exp = 0;
+  /*for(order in data.orders.nodes){
+    used  += order.injections;
+    arrived += order.vaccinationsByOrdersId.totalCount;
+  }*/
+  data.orders.nodes.forEach(function(order){
+    var injections = order.injections;
+    var used_from = order.vaccinationsByOrdersId.totalCount
+    if(injections === used_from){
+
+      console.log("Order "  + order.id + " is completely used");
+    }
+    else if(!isFutureDate(order.expires)){
+      console.log("Order "  + order.id + " is expired");
+      unused_exp += (injections - used_from);
+    }
+    else{
+      unused_not_exp += (injections - used_from);
+    }
+    arrived  += injections;
+    used += used_from;
+  })
+
+  header.innerHTML = "Käytetyt: " + used + " Saapuneet: " + arrived + " Voimassaolevat käyttämättömät: " + unused_not_exp + " Käyttämättömät vanhentuneet: " + unused_exp;
+}
+function vaccinationsLeftQuery(){
   var q  = `query usageQuery {
     orders {
       nodes {
+        id
         expires
         healthCareDistrict
         injections
@@ -35,7 +74,7 @@ function ordersQuery(){
     body: JSON.stringify({query: q})
     })
     .then(r => r.json())
-    .then(data => console.log('data returned:', data));
+    .then(data => left(data));
 
 }
 
@@ -45,8 +84,10 @@ function ordersQuery(){
 function printData(data){
   data = data.data;
   //console.log(data)
-  console.log("Vaccinations count: " + data.vaccinations.totalCount)
-  console.log("Orders count: " + data.orders.totalCount)
+  //console.log("Vaccinations count: " + data.vaccinations.totalCount)
+  //console.log("Orders count: " + data.orders.totalCount)
+  document.getElementById("total-orders").innerHTML = "Orders count: " + data.orders.totalCount;
+  document.getElementById("total-vaccinations").innerHTML = "Vaccinations count: " + data.vaccinations.totalCount;
 
 }
 function querySum(){
@@ -101,3 +142,4 @@ var sumquery = `query SumQuery {
 //query(q2);
 //query(sumquery);
 querySum();
+vaccinationsLeftQuery();
