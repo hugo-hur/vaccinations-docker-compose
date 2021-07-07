a0ef4631f5bca1ed5f9ea992c37e1859184f4879
Hugo Hurskainen <hugo.hurskainen@gmail.com>
2021-07-07 12:23:00 +0300

    Added database configuration files

---
+++ postgraphile/db/00-create-tables.sql
@@ -0,0 +1,33 @@
+--
+-- vaccin_impl schema is the private implementation schema
+--
+
+create schema vaccin_impl;
+
+-- json tables used to import json files to the database
+create table orders_j ("json" jsonb);
+create table vaccinations_j ("json" jsonb);
+
+-- actual data tables that will be populated from json tables with a stored
+-- procedure
+create table orders (
+ id                   uuid primary key
+,order_number         int not null
+,responsible_person   text not null
+,health_care_district text not null
+,vaccine              text not null
+,injections           int not null
+,arrived              timestamp with time zone not null
+,expires              timestamp with time zone not null
+);
+
+create index orders_index1 on orders (health_care_district);
+
+create table vaccinations (
+ id               uuid primary key
+,orders_id        uuid not null references orders(id)
+,gender           text not null
+,vaccination_date timestamp with time zone not null
+);
+
+create index vaccinations_index1 on vaccinations (orders_id);
---
+++ postgraphile/db/03-create-user.sql
@@ -0,0 +1,5 @@
+create user vaccin_user with encrypted password 'vaccin_user';
+grant usage on schema vaccin_impl to vaccin_user;
+grant select on vaccin_impl.orders to vaccin_user;
+grant select on vaccin_impl.vaccinations to vaccin_user;
+alter role vaccin_user set search_path to vaccin_impl;
