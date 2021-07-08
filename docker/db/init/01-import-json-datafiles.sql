2c7d6fd7721a987a6bb4b8cf3b6e49f629a3151b
Hugo Hurskainen <hugo.hurskainen@gmail.com>
2021-07-08 09:30:35 +0300

    Renamed psql to sql file in order to make postgres execute it

---
+++ docker/db/init/01-import-json-datafiles.sql
@@ -0,0 +1,5 @@
+\copy orders_j from '/usr/postgraphile/datafiles/Antiqua.source'
+\copy orders_j from '/usr/postgraphile/datafiles/SolarBuddhica.source'
+\copy orders_j from '/usr/postgraphile/datafiles/Zerpfy.source'
+
+\copy vaccinations_j from '/usr/postgraphile/datafiles/vaccinations.source'
