35100d746367efc96090c61660e60b4e4da0b6d4
Hugo Hurskainen <hugo.hurskainen@gmail.com>
2021-07-15 09:35:26 +0300

    Starting to add graphjing of expired vaccines

--- nginx/html/test.js
+++ nginx/html/test.js
@@ -106,6 +106,66 @@ function querySum(){
 
 }
 
+function addMonth(date){
+  time2 = new Date(date.getTime());
+  time2 = new Date(time2.setMonth(time2.getMonth()+1));
+  return time2;
+}
+
+async function printExpired(data){
+  var time1 = new Date('2021-01-01T00:00:00+02:00');//Jan
+  var expiries = [];
+  for(i = 0; i< 12; i++){
+    time2 = addMonth(time1);
+    console.log(time1.toISOString() + "  -  " + time2.toISOString())
+    var json = await queryExpired(time1.toISOString(), time2.toISOString());
+    console.log(json.data.orders.totalCount);
+    expiries.push(json.data.orders.totalCount)
+    time1 = time2;
+    addExpiryData((i+1).toString(), json.data.orders.totalCount);
+  }
+  console.log(expiries);
+  
+
+}
+
+function queryExpired(start, end){
+  var queryex = `query MyQuery {
+    orders(
+      condition: {
+        expiresEarlierThan: "%ts%" 
+        expiresLaterThan: "%later%"
+      }
+    )
+    {
+      totalCount
+      nodes{
+        vaccine
+        injections
+        vaccinationsByOrdersId {
+          totalCount
+        }
+      }
+    }
+  }`
+  queryex = queryex.replace("%ts%", start);
+  queryex = queryex.replace("%later%", end);
+  //console.log(queryex);
+
+  return fetch('http://localhost:5000/graphql', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+    body: JSON.stringify({query: queryex})
+    })
+    .then(r => r.json())
+    //.then(data => printExpired(data));
+   // .then(data => console.log(data.vaccinations));
+
+
+}
 
  var q2  = `query MyQuery {
     vaccinations {
@@ -138,8 +198,31 @@ var sumquery = `query SumQuery {
     totalCount
   }
 }`
+/*#arrivedEarlierThan: "2021-01-04 00:00:00+02:00"
+      #arrivedLaterThan: "2021-01-03 00:00:00+02:00"
+      #healthCareDistrict: "TAYS"*/
 
+var q = `query MyQuery {
+  orders(
+    condition: {
+      expiresEarlierThan: "2021-02-03 00:00:00+02:00" 
+      
+    }
+  )
+	{
+    totalCount
+    nodes{
+      vaccine
+      injections
+      vaccinationsByOrdersId {
+        totalCount
+      }
+    }
+  }
+}`
 //query(q2);
 //query(sumquery);
-querySum();
-vaccinationsLeftQuery();
+
+//querySum();
+//vaccinationsLeftQuery();
+printExpired();
